1ee860c879c07501ae1e38b2fd5acc1c0fc30528
JasperLefever <jasper.lefever@telenet.be>
2024-05-25 13:13:33 +0200

    removed alerrt

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/structure/navbar/Navbar.tsx";
 import Footer from "./components/structure/footer/Footer.tsx";
-import Alert from "./components/state/Alert.tsx";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Alert type={"info"}>Work in progress</Alert>
       <div className="px-2 py-4 flex-grow">
         <Outlet />
       </div>
